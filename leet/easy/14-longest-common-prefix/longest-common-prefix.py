a52cdb86d6daa0c74550b3caa2364f9ce557ddf0
s.repnikov <s.repnikov@maximatelecom.ru>
2024-01-05 19:16:27 +0300

    14 Longest common prefix is done.

--- leet/easy/14-longest-common-prefix/longest-common-prefix.py
+++ leet/easy/14-longest-common-prefix/longest-common-prefix.py
@@ -4,31 +4,16 @@ from typing import List
 class Solution:
 
     @staticmethod
-    def next_index(start: int, keep_checking: list[bool]) -> int:
-        for i in range(start, len(keep_checking)):
-            if keep_checking[i]:
-                return i
-
-        return -1
-
-    @staticmethod
     def longestCommonPrefix(strs: List[str]) -> str:
-        max_len = max(list(map(len, strs)))
+        shortest_string = min(sorted(strs, key=len, reverse=True))
 
-        keep_checking = []
-        for i in range(0, len(strs)):
-            keep_checking.append(True)
+        while shortest_string != "":
+            if all(list(map(lambda x: x.startswith(shortest_string), strs))):
+                return shortest_string
+            else:
+                shortest_string = shortest_string[:-1]
 
-        substr = ""
-
-        while all(keep_checking):
-
-            index = Solution.next_index(0, keep_checking)
-            current_letter = strs[index][len(substr)]
-            for i in range(index, len(strs)):
-                if keep_checking[i]:
-
-        return "fl"
+        return ""
 
 
 def main():
