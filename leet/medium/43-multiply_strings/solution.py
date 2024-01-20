3dcd2461cabbb0cf2561f255f44f6c7b75aca17e
s.repnikov <s.repnikov@maximatelecom.ru>
2024-01-20 14:34:31 +0300

    43 Multiply strings in progress 2.

--- leet/medium/43-multiply_strings/solution.py
+++ leet/medium/43-multiply_strings/solution.py
@@ -1,9 +1,16 @@
-import math
-
-
 class Solution:
+
+    #   0 1 2
+    #   1 2 3
+
+    #     1 0
+    #     3 4
+
+    # 0 1 2 3
+    # 3 2 1 0
+    # 4 1 8 2
     @staticmethod
-    def addStrings(num1: str, num2: str) -> str:
+    def multiplyStrings(num1: str, num2: str) -> str:
         lower = num1
         upper = num2
 
@@ -11,32 +18,33 @@ class Solution:
             lower = num2
             upper = num1
 
-        res = [0 for _ in range(0, len(upper))]
-        shift = len(upper) - 1
+        res = [0 for _ in range(0, len(upper) + len(lower))]
 
-        for lower_index, lower_digit in enumerate(reversed(lower)):
-            transfer = False
-            for upper_index, upper_digit in enumerate(reversed(upper)):
-                temp_mul = int(lower_digit) * int(upper_digit)
-                res_index = shift - upper_index
-                temp_res = temp_mul + res[res_index] + (1 if transfer else 0)
-                if temp_res >= 10:
-                    transfer = True
-                    temp_res = temp_res - 10
+        transit = 0
+        for l_i, l_e in (list(enumerate(reversed(lower)))):
+            for u_i, u_e in (list(enumerate(reversed(upper)))):
+                mul = int(l_e) * int(u_e)
+                curr_res = mul + transit + res[l_i + u_i]
+
+                if curr_res >= 10:
+                    transit = curr_res // 10
+                    curr_res = curr_res - transit * 10
                 else:
-                    transfer = False
+                    transit = 0
 
-                res[upper_index - shift] = temp_res
-                pass
+                res[l_i + u_i] = curr_res
+
 
         return ''.join(map(str, reversed(res)))
 
 
 def main():
-    assert Solution.addStrings(str(123), str(2)) == '246'  # simplest case
-    assert Solution.addStrings(str(123), str(4)) == '492'  # simplest case
-    assert Solution.addStrings(str(123), str(34)) == '4182'  # simplest case
-    assert Solution.addStrings(str(123), str(456)) == '56088'  # simplest case
+    # assert Solution.multiplyStrings(str(123), str(2)) == '246'  # simplest case
+    # assert Solution.multiplyStrings(str(123), str(4)) == '492'  # simplest case
+    # assert Solution.multiplyStrings(str(123), str(34)) == '4182'  # simplest case
+    # assert Solution.multiplyStrings(str(1234), str(456)) == '562704'  # simplest case
+    assert Solution.multiplyStrings(str(33), str(33)) == '56088'  # simplest case
+    assert Solution.multiplyStrings(str(123), str(456)) == '56088'  # simplest case
 
 
 if __name__ == "__main__":
