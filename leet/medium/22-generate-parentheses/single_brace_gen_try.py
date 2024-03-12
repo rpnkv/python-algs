3f519ee56d87de459b2866890a189d29b20d2e58
s.repnikov <s.repnikov@maximatelecom.ru>
2024-03-12 19:32:57 +0300

    22 done

--- leet/medium/22-generate-parentheses/single_brace_gen_try.py
+++ leet/medium/22-generate-parentheses/single_brace_gen_try.py
@@ -1,12 +1,13 @@
+# проверка должна быть после изменения стейта: в текущем цикле, или в следующем
 def generate(n: int, curr: int, acc: str) -> str:
-    if curr != n:
-        return generate(n, curr + 1, acc + ')')
-    else:
+    if curr == n:
         return acc
+    else:
+        return generate(n, curr + 1, acc + ')')
 
 
 def main():
-    print(generate(5, 0, ''))
+    assert generate(5, 0, '') == ')))))'
 
 
 if __name__ == '__main__':
--- leet/medium/22-generate-parentheses/solution.py
+++ leet/medium/22-generate-parentheses/solution.py
@@ -1,27 +1,52 @@
 from typing import List
 
 
+# frame 1: (2, 1, 0, '(') if 2 call generate(2, 1, 0, '((') frame 2
+# frame 2: (2, 2, 0, '((') if 3 call generate(2, 2, 1, '(()') frame 3
+# frame 3: (2, 2, 1, '(()') if 3 call generate(2, 2, 2, '(())') frame 4
+# frame 4: (2, 2, 2, '(())') if 1 --print('(())') method is over return to frame 3
+# frame 3: method is over return to frame 2
+# frame 2: method is over return to frame 1
+# frame 1: (2, 1, 0, '(') if 3 call generate (2, 1, 1, '()') frame 5
+# frame 5: (2, 1, 1, '()') if 2 call generate (2, 2, 1, '()(') frame 6
+# frame 6: (2, 2, 1, '()(') if 3 call generate (2, 2, 2, '()()') frame 7
+# frame 7: (2, 2, 2, '()()') if 1 print('()()') return to frame 6
+# frame 6: method is over return to frame 5
+# frame 5: if 3 not match method is over return to frame 1
+# frame 3: method is over
 class Solution:
+    res = []
 
-    @staticmethod
-    def generate(total: int, left: int, right: int, acc: str) -> List[str]:
-        if left != total - 1:
-            return Solution.generate(6, left + 1, right, acc + '(')
-        else:
-            if right != total -1:
-                
-        if right != total - 1:
-            pass
+    def generate(self, total: int, left: int, right: int, acc: list[str]) -> List[str]:
+        # if 1
+        if left == right == total:
+            self.res.append(''.join(acc))
 
-    @staticmethod
-    def generateParenthesis(n: int) -> List[str]:
-        list(Solution.generate(n, 0, 0, ''))
+        # if 2
+        if left != total:
+            acc.append('(')
+            self.generate(total, left + 1, right, acc)
+            acc.pop()
+
+        # if 3
+        if right != total and right < left:
+            acc.append(')')
+            self.generate(total, left, right + 1, acc)
+            acc.pop()
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.res = []
+        self.generate(n, 1, 0, ['('])
+        return self.res
 
 
 def main():
-    assert Solution().generateParenthesis(1) == ["()"]
-    assert Solution().generateParenthesis(2) == ["(())", "()()"]
-    assert Solution().generateParenthesis(3) == ["((()))", "(()())", "(())()", "()(())", "()()()"]
+    s1 = Solution()
+    assert s1.generateParenthesis(1) == ["()"]
+    # assert Solution().generateParenthesis(2) == ["()()", "(())"]
+
+    s3 = Solution()
+    assert s3.generateParenthesis(3) == ["((()))", "(()())", "(())()", "()(())", "()()()"]
 
 
 if __name__ == '__main__':
