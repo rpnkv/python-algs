4a071247b415efbeab989762c03a5cc046fe6ea2
s.repnikov <s.repnikov@maximatelecom.ru>
2025-10-28 17:53:13 +0300

    common "ListNode" class moved to "common" folder

---
+++ leet/src/common/ListNode.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __str__(self):
+        next_repr = "" if self.next is None else str(self.next)
+        return f"{self.val} + {next_repr}"
+
+    def __eq__(self, __value):
+        return super().__eq__(__value)
+
+
+def to_linked_list(the_list: list[int]) -> Optional[ListNode]:
+    if len(the_list) == 1:
+        return ListNode(val=the_list[0])
+    elif len(the_list) == 0:
+        return None
+
+    head = ListNode(val=the_list[0])
+    tail = head
+
+    for value in the_list[1:]:
+        tail_new = ListNode(val=value, next=None)
+        tail.next = tail_new
+        tail = tail_new
+
+    return head
---
+++ leet/src/hard/p23_merge_k_sorted_lists_naive.py
@@ -0,0 +1,9 @@
+# Definition for singly-linked list.
+from typing import List, Optional
+
+from common.ListNode import ListNode
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        pass
