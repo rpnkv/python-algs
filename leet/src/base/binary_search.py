ccc5384591b263608402d14ce3e00ce4bb0d52a0
s.repnikov <s.repnikov@maximatelecom.ru>
2025-10-28 08:43:20 +0300

    better naming, comments added

--- leet/src/base/binary_search.py
+++ leet/src/base/binary_search.py
@@ -1,9 +1,9 @@
 """Binary Search Algorithm"""
 
 
-def _resolve_new_boundaries(arr, target, left: int, right: int) -> tuple[int, int]:
+def _narrow_boundaries(arr, target, left: int, right: int) -> tuple[int, int]:
     """
-    Encapsulates boundaries shift logic.
+        Encapsulates boundaries shift logic.
     """
     mid = (left + right) // 2
     if arr[mid] == target:
@@ -20,16 +20,15 @@ def _resolve_new_boundaries(arr, target, left: int, right: int) -> tuple[int, in
 
 def search_while(arr, target) -> int:
     """
-    Binary search implementation, using "while" loop.
+        Binary search implementation, using "while" loop.
     """
     if len(arr) == 0:
         return -1
 
-    left = 0
-    right = len(arr) - 1
+    left, right = 0, len(arr) - 1
 
     while left != right:
-        left, right = _resolve_new_boundaries(arr, target, left, right)
+        left, right = _narrow_boundaries(arr, target, left, right)
 
     if arr[left] == target:
         return left
