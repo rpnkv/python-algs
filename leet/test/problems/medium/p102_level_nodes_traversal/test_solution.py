649930d012758944cbd38ace923d46f06e7ce13c
s.repnikov <s.repnikov@maximatelecom.ru>
2025-11-26 16:48:27 +0300

    p102 implementation sketch

---
+++ leet/test/problems/medium/p102_level_nodes_traversal/test_solution.py
@@ -0,0 +1,21 @@
+from common.tree_node import TreeNode
+from problems.medium.p102_level_nodes_traversal.solution import Solution
+
+
+def test():
+    sol = Solution()
+    input_tree = TreeNode(
+        val=3,
+        left=TreeNode(
+            9
+        ),
+        right=TreeNode(
+            20,
+            left=15,
+            right=7
+        )
+    )
+
+    expected_output=[[3],[9,20],[15,7]]
+
+    assert sol.levelOrder(input_tree) == expected_output
