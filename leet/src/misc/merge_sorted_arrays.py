44489e56e9e90ac26ff35e33319263493dacf586
s.repnikov <s.repnikov@maximatelecom.ru>
2025-12-01 12:52:48 +0300

    merge two sorted arrays done

--- leet/src/misc/merge_sorted_arrays.py
+++ leet/src/misc/merge_sorted_arrays.py
@@ -4,4 +4,22 @@ This file contains an implementation of a part of 'merge sort' algorithm, perfor
 
 
 def merge(left: list[int], right: list[int]) -> list[int]:
-    return []
+    left_index, right_index = 0, 0
+
+    result = []
+
+    while left_index != len(left) and right_index != len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    if left_index != len(left):
+        result += left[left_index:]
+
+    if right_index != len(right):
+        result+=right[right_index:]
+
+    return result
--- leet/test/misc/test_merge_sorted_arrays.py
+++ leet/test/misc/test_merge_sorted_arrays.py
@@ -4,8 +4,8 @@ Presumable corner cases:
 1. Left is empty
 2. Right is empty
 3. Both are empty
-4. Both are distinct and equal size
-5. Both are completely equal
+4. Both are completely equal
+    not implemented 4. Both are distinct and equal size
 """
 import pytest
 
@@ -14,8 +14,18 @@ from misc.merge_sorted_arrays import merge
 
 @pytest.mark.parametrize(
     argnames=["left", "right", "expected_output"],
-    argvalues=[],
-    ids=[],
+    argvalues=[
+        ([], [*range(5)], [*range(5)]),
+        ([*range(6)], [], [*range(6)]),
+        ([], [], []),
+        ([*range(3)], [*range(3)], [0, 0, 1, 1, 2, 2])
+    ],
+    ids=[
+        "left is empty",
+        "right is empty",
+        "both are empty",
+        "both are completely equal"
+    ],
 )
 def test(left, right, expected_output):
     assert merge(left, right) == expected_output
