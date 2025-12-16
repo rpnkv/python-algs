5a7bf9e96ae1284646af5508a7f824acc44acf05
s.repnikov <s.repnikov@maximatelecom.ru>
2025-12-16 11:25:34 +0300

    advanced solution for problem 88

--- leet/test/problems/easy/p88_merge_sorted_array/test_solution.py
+++ leet/test/problems/easy/p88_merge_sorted_array/test_solution.py
@@ -1,9 +1,10 @@
-from problems.easy.p88_merge_sorted_adday.solution_expected import Solution
+from problems.easy.p88_merge_sorted_adday.solution_expected import Solution as SolExpected
+from problems.easy.p88_merge_sorted_adday.solution_advanced import Solution as SolAdvanced
 
-sol = Solution()
+sol = SolExpected()
+sol_advanced = SolAdvanced()
 
-
-def test0():
+def test_expected():
     nums1 = [1, 2, 3, 0, 0, 0]
     m = 3
     nums2 = [2, 5, 6]
@@ -12,5 +13,18 @@ def test0():
     expected_output = [1, 2, 2, 3, 5, 6]
 
     sol.merge(nums1, m, nums2, n)
+
+    assert nums1 == expected_output
+
+
+def test_advanced():
+    nums1 = [1, 2, 3, 0, 0, 0]
+    m = 3
+    nums2 = [2, 5, 6]
+    n = 3
+
+    expected_output = [1, 2, 2, 3, 5, 6]
+
+    sol_advanced.merge(nums1, m, nums2, n)
 
     assert nums1 == expected_output
