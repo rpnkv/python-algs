45de4c887a2f947c420e2e75446a2d7a9fb3e974
s.repnikov <s.repnikov@maximatelecom.ru>
2025-12-17 21:53:19 +0300

    before implementing in order traversal

--- base/test/algs/fundamental/trees/test_in_order_traversal.py
+++ base/test/algs/fundamental/trees/test_in_order_traversal.py
@@ -21,11 +21,11 @@ def _init_test_tree() -> TreeNode:
 @pytest.mark.parametrize(
     argnames=["traversing_tree_as_array", "expected_values"],
     argvalues=[
-        # ([1, None, 2, None, None, 3], [1, 3, 2])
-        ([1, 2, 3], [2, 1, 3])
+        #([1, None, 2, None, None, 3], [1, 3, 2]),
+        ([1, 2, 3], [2, 1, 3]),
     ],
     ids=["example 1"]
 )
 def test_parametrised(traversing_tree_as_array, expected_values):
-    root = TreeNode.froThm_level_order_array(traversing_tree_as_array)
+    root = TreeNode.from_level_order_array(traversing_tree_as_array)
     assert traverse(root) == expected_values
