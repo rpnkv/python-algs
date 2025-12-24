451611cc2287a803a5085c4c21dea1004150c21d
s.repnikov <s.repnikov@maximatelecom.ru>
2025-12-24 20:35:55 +0300

    re-write expected + test cases set extended

--- leet/src/problems/easy/p88_merge_sorted_array/solution_expected.py
+++ leet/src/problems/easy/p88_merge_sorted_array/solution_expected.py
@@ -3,5 +3,25 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        pass
+        copy = nums1[0:m]
 
+        l1 = l2 = res = 0
+
+        while l1 < m and l2 < n:
+            if copy[l1] < nums2[l2]:
+                nums1[res] = copy[l1]
+                l1 += 1
+            else:
+                nums1[res] = nums2[l2]
+                l2 += 1
+            res += 1
+
+        while l1 < m:
+            nums1[res] = copy[l1]
+            l1 += 1
+            res += 1
+
+        while l2 < n:
+            nums1[res] = nums2[l2]
+            l2 += 1
+            res += 1
--- leet/test/problems/easy/p88_merge_sorted_array/test_solution.py
+++ leet/test/problems/easy/p88_merge_sorted_array/test_solution.py
@@ -4,36 +4,27 @@ from problems.easy.p88_merge_sorted_array.solution_advanced import Solution as S
 from problems.easy.p88_merge_sorted_array.solution_expected import Solution as SolExpected
 
 TEST_DATA = [
-    pytest.param([1, 2, 3, 0, 0, 0], [2, 5, 6], [1, 2, 2, 3, 5, 6], id="case1")
+    pytest.param([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], [1, 2, 2, 3, 5, 6], id="regular case"),
+    pytest.param([0, 0, 0], 0, [2, 5, 6], [2, 5, 6], id="left is empty"),
+    pytest.param([2, 5, 6], 3, [], [2, 5, 6], id="right is empty"),
+    pytest.param([], 0, [], [], id="both are empty"),
+    pytest.param([1], 1, [], [1], id="one in the left"),
+    pytest.param([0], 0, [1], [1], id="one in the right"),
 ]
 
 sol = SolExpected()
 sol_advanced = SolAdvanced()
 
 
-@pytest.mark.parametrize(["arr1", "arr2", "expected"], TEST_DATA)
-def test_expected(arr1, arr2, expected):
-    nums1 = [1, 2, 3, 0, 0, 0]
-    m = 3
-    nums2 = [2, 5, 6]
-    n = 3
+@pytest.mark.parametrize(["arr1", "arr1_len", "arr2", "expected"], TEST_DATA)
+def test_expected(arr1, arr1_len, arr2, expected):
+    sol.merge(arr1, arr1_len, arr2, len(arr2))
 
-    expected_output = [1, 2, 2, 3, 5, 6]
-
-    sol.merge(nums1, m, nums2, n)
-
-    assert nums1 == expected_output
+    assert arr1 == expected
 
 
-@pytest.mark.parametrize(["arr1", "arr2", "expected"], TEST_DATA)
-def test_advanced(arr1, arr2, expected):
-    nums1 = [1, 2, 3, 0, 0, 0]
-    m = 3
-    nums2 = [2, 5, 6]
-    n = 3
+@pytest.mark.parametrize(["arr1", "arr1_len", "arr2", "expected"], TEST_DATA)
+def test_advanced(arr1, arr1_len, arr2, expected):
+    sol_advanced.merge(arr1, arr1_len, arr2, len(arr2))
 
-    expected_output = [1, 2, 2, 3, 5, 6]
-
-    sol_advanced.merge(nums1, m, nums2, n)
-
-    assert nums1 == expected_output
+    assert arr1 == expected
