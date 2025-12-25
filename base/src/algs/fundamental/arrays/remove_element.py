81f9353b763d06a85f61d5ed6867f9f09cd14a99
s.repnikov <srpnkv.dev@gmail.com>
2025-12-25 14:18:41 +0300

    'remove element' implementation

--- base/src/algs/fundamental/arrays/remove_element.py
+++ base/src/algs/fundamental/arrays/remove_element.py
@@ -1,5 +1,12 @@
 from typing import List
 
 
-def remove_element(nums: List[int], val: int) -> int:
-    return -1
+def remove_element(nums: List[int], invalid_val: int) -> int:
+    l = 0   # any element to the left from this position is valid
+            # any element to the right from this position and to the left from 'r' position is invalid
+
+    for r in range(len(nums)): #any element to the right may be valid or invalid
+        if nums[r] != invalid_val:
+            nums[l] = nums[r]
+            l += 1
+    return l
