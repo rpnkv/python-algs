074b28dab113a03b8cadcaaa54ecf50a21916092
s.repnikov <s.repnikov@maximatelecom.ru>
2025-12-25 14:26:55 +0300

    leetcode reference in solution. test removed due to duplication

--- leet/src/problems/easy/p27_remove_element/solution.py
+++ leet/src/problems/easy/p27_remove_element/solution.py
@@ -4,20 +4,5 @@ from typing import List
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         """
-        В этой реализации у нас 3 зоны:
-        - по левую сторону от левого -- не подлежащие удалению;
-        - между левым и правым "серая зона"?;
-        - по правую сторону от правого -- не проверенные.
-
-        Что меня смущает? Почему мы слепо двигаем элементы на левый указатель, если они не "удаляемые"?
-        
+            refers to base array exercise 'remove element'
         """
-
-        left = 0
-
-        for right in range(len(nums)):
-            if nums[right] != val:
-                nums[left] = nums[right]
-                left += 1
-
-        return left
