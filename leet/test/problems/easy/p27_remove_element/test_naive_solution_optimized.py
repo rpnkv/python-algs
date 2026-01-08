365b60d25bfb3403cef48e0d820fb9ac3ac88450
s.repnikov <srpnkv.dev@gmail.com>
2026-01-08 21:03:02 +0500

    'P27 Remove element' naive solution optimized

---
+++ leet/test/problems/easy/p27_remove_element/test_naive_solution_optimized.py
@@ -0,0 +1,21 @@
+import pytest
+
+from problems.easy.p27_remove_element.naive_solution_optimized import Solution
+
+sol = Solution()
+
+TEST_CASES = [
+    pytest.param([], 0, [], id="empty input"),
+    pytest.param([2, 2], 2, [], id="empty result"),
+    pytest.param([3, 2, 2], 3, [2, 2], id="removed from beginning"),
+    pytest.param([3, 3, 2, 2], 3, [2, 2], id="removed two from beginning"),
+    pytest.param([2, 2, 3], 3, [2, 2], id="removed from ending"),
+    pytest.param([2, 2, 3, 3], 3, [2, 2], id="removed two from ending"),
+    pytest.param([3, 2, 2, 3], 3, [2, 2], id="base case"),
+]
+
+
+@pytest.mark.parametrize(["nums", "val", "expected_output"], TEST_CASES)
+def test(nums, val, expected_output):
+    lngth = sol.removeElement(nums, val)
+    assert nums[:lngth] == expected_output
