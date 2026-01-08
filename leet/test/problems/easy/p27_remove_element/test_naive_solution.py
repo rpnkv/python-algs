643be5dfe81e229eb638ea35c6b42b8ffb2fd4f1
s.repnikov <srpnkv.dev@gmail.com>
2026-01-08 20:17:23 +0500

    'P27 Remove element' more test cases

--- leet/test/problems/easy/p27_remove_element/test_naive_solution.py
+++ leet/test/problems/easy/p27_remove_element/test_naive_solution.py
@@ -5,7 +5,13 @@ from problems.easy.p27_remove_element.naive_solution import Solution
 sol = Solution()
 
 TEST_CASES = [
-    pytest.param([3, 2, 2, 3], 3, [2, 2], id="base case")
+    pytest.param([], 0, [], id="empty input"),
+    pytest.param([2, 2], 2, [], id="empty result"),
+    pytest.param([3, 2, 2], 3, [2, 2], id="removed from beginning"),
+    pytest.param([3, 3, 2, 2], 3, [2, 2], id="removed two from beginning"),
+    pytest.param([2, 2, 3], 3, [2, 2], id="removed from ending"),
+    pytest.param([2, 2, 3, 3], 3, [2, 2], id="removed two from ending"),
+    pytest.param([3, 2, 2, 3], 3, [2, 2], id="base case"),
 ]
 
 
