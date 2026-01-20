7ecc2d4acdf8f72a8175b1c1ba30f7401728961d
s.repnikov <srpnkv.dev@gmail.com>
2026-01-20 21:21:28 +0300

    p79 'word search' backbone

---
+++ leet/src/problems/medium/p79_word_search/solution_naive.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ...
---
+++ leet/test/problems/medium/p79_word_search/test_solution_naive.py
@@ -0,0 +1,19 @@
+from typing import List
+
+import pytest
+
+from problems.medium.p79_word_search.solution_naive import Solution
+
+sol = Solution()
+
+
+@pytest.mark.parametrize(
+    argnames=["board", "word", "expected_output"],
+    argvalues=[
+        ([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCCED", True),
+        ([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "SEE", True),
+        ([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCB", True),
+    ]
+)
+def test_solution(board: List[List[str]], word: str, expected_output: bool):
+    assert sol.exist(board, word) == expected_output
