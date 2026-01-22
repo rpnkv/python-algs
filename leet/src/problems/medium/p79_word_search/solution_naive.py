dd17c66f1f13e3babfc9ebc6ed4023e478c054d9
s.repnikov <srpnkv.dev@gmail.com>
2026-01-22 19:43:16 +0300

    p79 'word search' naive solution works

--- leet/src/problems/medium/p79_word_search/solution_naive.py
+++ leet/src/problems/medium/p79_word_search/solution_naive.py
@@ -2,8 +2,7 @@ from typing import List
 
 
 class Solution:
-
-    def _search(self, board, i, j, word, n) -> bool:
+    def _search(self, board, i, j, word, n, visited: list[list[bool]]) -> bool:
         if i < 0:
             return False
         elif i >= len(board):
@@ -13,38 +12,44 @@ class Solution:
         elif j >= len(board[i]):
             return False
 
-
         if board[i][j] == word[n]:
-            if self.visited[i][j]:
+            if visited[i][j]:
                 return False
             else:
-                self.visited[i][j] = True
+                visited[i][j] = True
 
             if n == len(word) - 1:
                 return True
-            else:
-                return any(
-                    [
-                        self._search(board, i, j + 1, word, n + 1),
-                        self._search(board, i, j - 1, word, n + 1),
-                        self._search(board, i + 1, j, word, n + 1),
-                        self._search(board, i - 1, j, word, n + 1),
-                    ]
-                )
+
+            if self._search(board, i, j + 1, word, n + 1, visited.copy()):  # go right
+                return True
+
+            if self._search(board, i, j - 1, word, n + 1, visited.copy()):  # go_left
+                return True
+
+            if self._search(board, i + 1, j, word, n + 1, visited.copy()):  # go_down
+                return True
+
+            if self._search(board, i - 1, j, word, n + 1, visited.copy()):  # go_up
+                return True
+
+            visited[i][j] = False
+            return False
+
+
         else:
             return False
 
     def exist(self, board: List[List[str]], word: str) -> bool:
         for i in range(len(board)):
             for j in range(len(board[i])):
-                self.visited = []
+                visited = []
 
                 for x in range(len(board)):
-                    self.visited.append([False] * len(board[x]))
+                    visited.append([False] * len(board[x]))
 
-                res = self._search(board, i, j, word, 0)
+                res = self._search(board, i, j, word, 0, visited)
                 if res:
                     return True
-
 
         return False
