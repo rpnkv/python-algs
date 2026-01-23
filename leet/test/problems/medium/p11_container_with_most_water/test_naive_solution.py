9f6bc852538018474c5c3f648716d2eb512a6b91
s.repnikov <srpnkv.dev@gmail.com>
2026-01-23 15:16:21 +0300

    p11 'container with the most water' test for expected solution

--- leet/test/problems/medium/p11_container_with_most_water/test_naive_solution.py
+++ leet/test/problems/medium/p11_container_with_most_water/test_naive_solution.py
@@ -1,16 +1,19 @@
 import pytest
 
 from problems.medium.p11_container_with_most_water.naive_solution import Solution
+from problems.medium.p11_container_with_most_water.expected_solution import Solution as Solution_expected
 
-sol = Solution()
+TEST_CASES = [
+    pytest.param([1, 3, 1, 4], 6, id="basic case"),
+    pytest.param([1, 8, 6, 2, 5, 4, 8, 3, 7], 49, id="example case"),
+]
 
 
-@pytest.mark.parametrize(
-    argnames=["heights", "expected_square"],
-    argvalues=[
-        ([1, 3, 1, 4], 6),
-        ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
-    ]
-)
-def test(heights, expected_square):
+@pytest.mark.parametrize(["heights", "expected_square"], TEST_CASES)
+def test_naive(heights, expected_square):
+    sol = Solution()
     assert sol.maxArea(heights) == expected_square
+
+
+def test_expected(heights, expected_square):
+    sol = Solution_expected()
