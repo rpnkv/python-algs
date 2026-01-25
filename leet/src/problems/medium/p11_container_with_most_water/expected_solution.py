64a95106df46fa902503ea6c6e32de8e2e775776
s.repnikov <srpnkv.dev@gmail.com>
2026-01-25 13:49:28 +0300

    p11 'container with most water' expected solution

--- leet/src/problems/medium/p11_container_with_most_water/expected_solution.py
+++ leet/src/problems/medium/p11_container_with_most_water/expected_solution.py
@@ -3,4 +3,27 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        return - 1
+        l, r = 0, len(height) - 1
+
+        max_area = 0
+
+        while True:
+            current_width = r - l
+            current_height = min(height[l], height[r])
+
+            current_area = current_width * current_height
+            max_area = max(current_area, max_area)
+
+            if l + 1 == r:
+                break
+
+            if r -1 == l:
+                break
+
+            #if height[l] + 1 > height[r] - 1:
+            if height[l] > height[r]:
+                r -= 1
+            else:
+                l += 1
+
+        return max_area
--- leet/test/problems/medium/p11_container_with_most_water/test_solution.py
+++ leet/test/problems/medium/p11_container_with_most_water/test_solution.py
@@ -1,11 +1,12 @@
 import pytest
 
-from problems.medium.p11_container_with_most_water.naive_solution import Solution
 from problems.medium.p11_container_with_most_water.expected_solution import Solution as Solution_expected
+from problems.medium.p11_container_with_most_water.naive_solution import Solution
 
 TEST_CASES = [
     pytest.param([1, 3, 1, 4], 6, id="basic case"),
     pytest.param([1, 8, 6, 2, 5, 4, 8, 3, 7], 49, id="example case"),
+    pytest.param([8, 7, 2, 1], 7, id="case 24"),
 ]
 
 
@@ -15,5 +16,7 @@ def test_naive(heights, expected_square):
     assert sol.maxArea(heights) == expected_square
 
 
+@pytest.mark.parametrize(["heights", "expected_square"], TEST_CASES)
 def test_expected(heights, expected_square):
     sol = Solution_expected()
+    assert sol.maxArea(heights) == expected_square
