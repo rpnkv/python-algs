c00fbe7fcce2fa056809b61b30e819a37ac2c520
s.repnikov <srpnkv.dev@gmail.com>
2026-01-26 09:45:09 +0300

    p83 'remove duplicates from ll' -- test fixed

--- leet/test/problems/medium/p83_remove_dupl_from_sorted_ll/test_solution.py
+++ leet/test/problems/medium/p83_remove_dupl_from_sorted_ll/test_solution.py
@@ -1,21 +1,25 @@
 import pytest
 
+from common.list_node import ListNode
 from problems.easy.p83_remove_dupl_from_sorted_ll.solution import Solution
 
 sol = Solution()
 
 
 @pytest.mark.parametrize(
-    argnames=["input", "expected_output"],
+    argnames=["input_list", "expected_output_list"],
     argvalues=[
-        ([1]), ([1]),
-        ([1, 1]), ([1]),
-        ([1, 1, 1]), ([1]),
-        ([1, 1, 1, 2]), ([1, 2]),
-        ([1, 2, 2]), ([1, 2]),
-        ([1, 2, 2, 2]), ([1, 2]),
-        ([1, 2, 3]), ([1, 2, 3]),
+        ([1], [1]),
+        ([1, 1], [1]),
+        ([1, 1, 1], [1]),
+        ([1, 1, 1, 2], [1, 2]),
+        ([1, 2, 2], [1, 2]),
+        ([1, 2, 2, 2], [1, 2]),
+        ([1, 2, 3], [1, 2, 3]),
     ]
 )
 def test_solution(input_list, expected_output_list):
-    assert sol.deleteDuplicates(input_list) == expected_output_list
+    input_head = ListNode.to_linked_list(input_list)
+    expected_output_head = ListNode.to_linked_list(expected_output_list)
+
+    assert sol.deleteDuplicates(input_head) == expected_output_head
