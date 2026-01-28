225fb9e84f9693cdb8f44b181fc854db1a4242c3
s.repnikov <srpnkv.dev@gmail.com>
2026-01-28 12:33:03 +0300

    p83 'remove duplicates from ll' done

--- leet/src/problems/easy/p83_remove_dupl_from_sorted_ll/solution.py
+++ leet/src/problems/easy/p83_remove_dupl_from_sorted_ll/solution.py
@@ -4,5 +4,22 @@ from common.list_node import ListNode
 
 
 class Solution:
-    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:  ## 12 minutes
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+
+        26.01.26  12 minutes
+        28.01.26  4 minutes
+        """
+        current: ListNode = head
+
+        while current and current.next:
+            if current.val != current.next.val:
+                current = current.next
+            else:
+                temp_next: ListNode = current.next
+                while temp_next and temp_next.val == current.val:
+                    temp_next = temp_next.next
+
+                current.next = temp_next
+
         return head
