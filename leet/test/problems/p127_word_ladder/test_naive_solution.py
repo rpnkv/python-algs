470e0efba1cff35e7f6013364447abbca05ccdf1
s.repnikov <srpnkv.dev@gmail.com>
2026-01-31 12:29:48 +0300

    p127 'word ladder': 'example' test cases prepared

--- leet/test/problems/p127_word_ladder/test_naive_solution.py
+++ leet/test/problems/p127_word_ladder/test_naive_solution.py
@@ -1,7 +1,17 @@
+import pytest
+
 from problems.p127_word_ladder.solution import Solution
 
 sol = Solution()
 
 
-def test_solution():
-    assert sol.ladderLength("", "", []) == 0
+@pytest.mark.parametrize(
+    argnames=["begin_word", "end_word", "word_list", "expected_output"],
+    argvalues=[
+        ("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"], 5),
+        ("hit", "cog", ["hot", "dot", "dog", "lot", "log"], 0),
+    ],
+    ids=["example1", "example2"]
+)
+def test_solution(begin_word: str, end_word: str, word_list: list[str], expected_output: int):
+    assert sol.ladderLength(begin_word, end_word, word_list) == expected_output
