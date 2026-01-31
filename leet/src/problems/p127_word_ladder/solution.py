ccdd667192ac6d90175d4c1d2df2fea5d9da72b9
s.repnikov <srpnkv.dev@gmail.com>
2026-01-31 14:18:11 +0300

    p127 'word ladder': stopped working on the task to refresh my graph theory knowledge

--- leet/src/problems/p127_word_ladder/solution.py
+++ leet/src/problems/p127_word_ladder/solution.py
@@ -1,5 +1,53 @@
 from typing import List
 
+
 class Solution:
+    @staticmethod
+    def _check_if_one_letter_differs(current_word: str, checking_word: str) -> bool:
+        letters_differ = 0
+
+        current_letter_index = 0
+        while current_letter_index < len(current_word) and letters_differ < 2:
+            if current_word[current_letter_index] != checking_word[current_letter_index]:
+                letters_differ += 1
+
+            current_letter_index += 1
+
+        return letters_differ == 1
+
+    @staticmethod
+    def _define_possible_routes(current_word: str, current_options: list[str]) -> list[str]:
+        return [current_option for current_option in current_options if
+                Solution._check_if_one_letter_differs(current_word, current_option)]
+
+    @staticmethod
+    def _search_recursively(current_word: str, target_word: str, word_list: List[str], current_steps: int) -> int:
+        """
+        Top level steps:
+            1. Define possible routes for current word.
+            2. Check if some possible routes returns non-zero.
+
+        Implementation details:
+            1. Define possible routes.
+            Iterate over all possible routes and define those which match certain condition.
+
+            2. Check if some possible routes return "true".
+
+
+
+        """
+        if len(word_list) == 0:
+            return 0
+
+        if current_word == target_word:
+            return current_steps
+
+        possible_routes = Solution._define_possible_routes(current_word, word_list)
+
+        for possible_route in possible_routes:
+            Solution._search_recursively()
+
+        raise NotImplemented
+
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        return -1
+        return self.search_recursively(beginWord, endWord, wordList, 0)
--- leet/test/problems/p127_word_ladder/test_naive_solution.py
+++ leet/test/problems/p127_word_ladder/test_naive_solution.py
@@ -2,6 +2,45 @@ import pytest
 
 from problems.p127_word_ladder.solution import Solution
 
+
+@pytest.mark.parametrize(
+    argnames=["current_word", "checking_word", "expected_result"],
+    argvalues=[
+        # 1 letter
+        ("w", "w", False),
+        ("w", "x", True),
+        ("w", "a", True),
+        # 2 letters
+        ("ww", "ww", False),
+        ("ww", "wa", True),
+        ("ww", "aa", False),
+        ("ww", "aw", True),
+        # 3 letters
+        ("www", "www", False),
+        ("www", "wwa", True),
+        ("www", "waa", False),
+        ("www", "aaa", False),
+        ("www", "aww", True),
+        ("www", "aaw", False),
+        ("www", "waw", True),
+    ]
+)
+def test_check_if_one_letter_differs(current_word, checking_word, expected_result: bool):
+    assert Solution._check_if_one_letter_differs(current_word, checking_word) == expected_result
+
+@pytest.mark.parametrize(
+    argnames=["current_word", "all_routes", "possible_routes"],
+    argvalues=[
+        ("cat", [], []),
+        ("cat", ["car"], ["car"]),
+        ("cat", ["car", "cax"], ["car", "cax"]),
+        ("cat", ["car", "cax", "cor"], ["car", "cax"]),
+    ]
+)
+def test_define_possible_routes(current_word: str, all_routes: list[str], possible_routes:list[str]):
+    assert Solution._define_possible_routes(current_word, all_routes) == possible_routes
+
+
 sol = Solution()
 
 
@@ -13,5 +52,5 @@ sol = Solution()
     ],
     ids=["example1", "example2"]
 )
-def test_solution(begin_word: str, end_word: str, word_list: list[str], expected_output: int):
+def test_solution_completely(begin_word: str, end_word: str, word_list: list[str], expected_output: int):
     assert sol.ladderLength(begin_word, end_word, word_list) == expected_output
