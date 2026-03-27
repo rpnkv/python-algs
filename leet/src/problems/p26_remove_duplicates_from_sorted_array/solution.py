0f199e255cfdc555a6e7f3ae1efdee69b5111360
s.repnikov <srpnkv.dev@gmail.com>
2026-03-27 10:56:55 +0300

    leetcode:p26:remove duplicates from sorted array:expected solution cleanup to repeat

--- leet/src/problems/p26_remove_duplicates_from_sorted_array/solution.py
+++ leet/src/problems/p26_remove_duplicates_from_sorted_array/solution.py
@@ -3,23 +3,3 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        left: int = None
-
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
-                left = i
-                break
-
-        else:
-            return len(nums)
-
-        right = left + 1
-
-        while right < len(nums):
-            if nums[right] != nums[left]:
-                left += 1
-                nums[left] = nums[right]
-
-            right += 1
-
-        return left + 1
