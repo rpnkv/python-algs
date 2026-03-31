27bc5cb77c9d336601fd5bd519dfdfc87a58ad21
s.repnikov <srpnkv.dev@gmail.com>
2026-03-31 22:19:07 +0300

    leetcode:p27:remove element:expected solution complete

--- leet/src/problems/p27_remove_element/expected_solution.py
+++ leet/src/problems/p27_remove_element/expected_solution.py
@@ -2,3 +2,19 @@ from typing import List
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        left = len(nums)
+
+        for i in range(len(nums)):
+            if nums[i] == val:
+                left = i
+                break
+
+        else:
+            return left
+
+        for right in range(left + 1, len(nums)):
+            if nums[right] != val:
+                nums[left] = nums[right]
+                left += 1
+
+        return left
