4aecaf552084a8d80473d187490b42d74a3cc7f8
s.repnikov <srpnkv.dev@gmail.com>
2026-03-31 22:22:11 +0300

    leetcode:p169:majority:expected solution cleanup

--- leet/src/problems/p169_majority_element/expected_solution.py
+++ leet/src/problems/p169_majority_element/expected_solution.py
@@ -2,16 +2,4 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        element = 0
-        count = 0
-
-        for i in nums:
-            if element == i:
-                count += 1
-            else:
-                if count == 0:
-                    element = i
-                else:
-                    count -= 1
-
-        return element
+        return - 1
