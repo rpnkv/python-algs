fc918be2f3a8a68bac6763d1039c1b354a8ae374
s.repnikov <srpnkv.dev@gmail.com>
2026-03-31 21:57:45 +0300

    leetcode:p88:merge sorted array:advanced solution complete

--- leet/src/problems/p88_merge_sorted_array/solution_advanced.py
+++ leet/src/problems/p88_merge_sorted_array/solution_advanced.py
@@ -7,5 +7,20 @@ class Solution:
               nums2: List[int],
               n: int
               ) -> None:
+        vacant_index = len(nums1) - 1
+        m_index = m - 1
+        n_index = n - 1
 
-        return
+        while m_index >= 0 and n_index >= 0:
+            if nums1[m_index] > nums2[n_index]:
+                nums1[vacant_index] = nums1[m_index]
+                m_index -= 1
+            else:
+                nums1[vacant_index] = nums2[n_index]
+                n_index -= 1
+
+            vacant_index -= 1
+
+        if n_index >= 0:
+            the_slice = slice(0, n_index + 1)
+            nums1[the_slice] = nums2[the_slice]
