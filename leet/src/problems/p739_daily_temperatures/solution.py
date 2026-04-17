8ed3a04033489111c2109125561a6ba79b7f1e4e
s.repnikov <srpnkv.dev@gmail.com>
2026-04-17 11:36:18 +0300

    leetcode:p739:daily temperatures:repeat

--- leet/src/problems/p739_daily_temperatures/solution.py
+++ leet/src/problems/p739_daily_temperatures/solution.py
@@ -3,15 +3,30 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # stack must keep temperatures in increasing order (higher at the bottom)
+        # pytest.param([75, 71, 69, 72], [0, 2, 1, 0], id="example 1 subarray"),
         stack = []
-        answer = [0] * len(temperatures)
+        answer = [0]*len(temperatures)
 
+        for i, t in enumerate(temperatures):
 
-        for i, el in enumerate(temperatures):
-            while stack and stack[-1][1] < el:
-                i_st, el_st = stack.pop()
+            while stack and stack[-1][1] < t:
+                i_st, t_st = stack.pop()
                 answer[i_st] = i - i_st
 
-            stack.append((i, el))
+            stack.append((i,t))
+
+        # 1: i=0, t=75
+        # stack: [(0, 75)], answer: [0,0,0,0]
+        #
+        # 2: i=1, t=71
+        # stack: [(0,75), (1,71)]
+        #
+        # 3: i=2, t=69
+        # stack: [(0,75), (1,71), (2,69)]
+        #
+        # 4: i=3, t=72
+        # stack:
+
 
         return answer
--- leet/test/problems/p739_daily_temperatures/test_solution.py
+++ leet/test/problems/p739_daily_temperatures/test_solution.py
@@ -4,6 +4,7 @@ from problems.p739_daily_temperatures.solution import Solution
 
 TEST_CASES = [
     pytest.param([73, 74, 75, 71, 69, 72, 76, 73], [1, 1, 4, 2, 1, 1, 0, 0], id="example 1"),
+    pytest.param([75, 71, 69, 72], [0, 2, 1, 0], id="example 1 subarray"),
     pytest.param([30, 60, 90], [1, 1, 0], id="example 3")
     #pytest.param([73, 74, 75, 71, 69, 72, 76, 73], [74, 75, 76, 72, 76, -1, -1], id="example 1"),
 ]
