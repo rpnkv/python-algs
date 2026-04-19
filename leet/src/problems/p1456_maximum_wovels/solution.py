92db2bd69bfa9eff87219d0832c4d16949624ead
Sergei Repnikov <theierus@gmail.com>
2026-04-19 13:56:20 +0300

    leetcode:p1456:maximum wovels in substring: done!

--- leet/src/problems/p1456_maximum_wovels/solution.py
+++ leet/src/problems/p1456_maximum_wovels/solution.py
@@ -1,16 +1,18 @@
 class Solution:
     def maxVowels(self, s: str, k: int) -> int:
-        count = 0
+        max_count = 0
         wovels = {'a', 'e', 'i', 'o', 'u'}
 
+        curr_count = 0
         for i, chr in enumerate(s):
             count_incr = 1 if chr in wovels else 0
 
-            if i > k:
+            if i >= k:
                 count_decr = -1 if s[i-k] in wovels else 0
             else:
                 count_decr = 0
 
-            count = max(count, count + count_incr + count_decr)
+            curr_count = curr_count + count_incr + count_decr
+            max_count = max(max_count, curr_count)
 
-        return count
+        return max_count
