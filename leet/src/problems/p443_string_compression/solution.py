7891f1b36c9b23f374b04a07ffc133bcce75316a
s.repnikov <srpnkv.dev@gmail.com>
2026-04-22 13:03:25 +0300

    leetcode:p443:string compression: attempt 1, doesnt work (algo doesn't commit last character count)

--- leet/src/problems/p443_string_compression/solution.py
+++ leet/src/problems/p443_string_compression/solution.py
@@ -3,6 +3,28 @@ from typing import List
 
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        raise NotImplementedError
+        # two pointers
+        # left points to the beginning of current substring with same characters
+        # right iterates
 
+        # at each iteration, we must determine, whether new character belongs to prev string (at l pos) or to new.
+        # in first case, we just iterate letters counter
+        # in second (new letter discovered) -- commit prev count, shift left pointer, initialize new count
 
+        output_str = ""
+        current_count = l = 0
+
+        for r, r_char in enumerate(chars):
+            if r_char == chars[l]:
+                current_count += 1
+            else:
+                if current_count == 1:
+                    output_str += chars[l]
+                else:
+                    output_str += chars[l]
+                    output_str += str(current_count)
+
+                current_count = 1
+                l = r
+
+        return len(output_str)
