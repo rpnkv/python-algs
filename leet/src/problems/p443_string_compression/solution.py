302fc954bb704045ca793db3387e1cec0d6b9dad
Sergei Repnikov <theierus@gmail.com>
2026-04-22 13:55:43 +0300

    leetcode:p1493:longest subarray of ones: code duplicates refactor

--- leet/src/problems/p443_string_compression/solution.py
+++ leet/src/problems/p443_string_compression/solution.py
@@ -2,27 +2,29 @@ from typing import List
 
 
 class Solution:
-    def compress(self, chars: List[str]) -> int:
-        output_str = ""
-        current_count = l = 0
 
-        for r, r_char in enumerate(chars):
-            if r_char == chars[l]:
-                current_count += 1
-            else:
-                if current_count == 1:
-                    output_str += chars[l]
-                else:
-                    output_str += chars[l]
-                    output_str += str(current_count)
+    def __init__(self):
+        self.output_str = ''
+        self.current_count = 0
+        self.l = 0
 
-                current_count = 1
-                l = r
-
-        if current_count == 1:
-            output_str += chars[l]
+    def commit(self, chars: List[str]) -> None:
+        if self.current_count == 1:
+            self.output_str += chars[self.l]
         else:
-            output_str += chars[l]
-            output_str += str(current_count)
+            self.output_str += chars[self.l]
+            self.output_str += str(self.current_count)
 
-        return len(output_str)
+        self.current_count = 1
+
+    def compress(self, chars: List[str]) -> int:
+        for r, r_char in enumerate(chars):
+            if r_char == chars[self.l]:
+                self.current_count += 1
+            else:
+                self.commit(chars)
+                self.l = r
+
+        self.commit(chars)
+
+        return len(self.output_str)
