4a632661ba0d29615dd2e22ab7433e61a135dec4
s.repnikov <srpnkv.dev@gmail.com>
2026-04-24 17:09:44 +0300

    base:binary search: done

--- base/src/algs/fundamental/search/binary_search.py
+++ base/src/algs/fundamental/search/binary_search.py
@@ -1,6 +1,22 @@
-def _search(arr: list[int], target: int, left: int, right: int) -> int:
-    
-
-
 def search(arr: list[int], target: int) -> int:
-    return _search(arr, target, 0, len(arr) - 1)
+    if len(arr) == 0:
+        return - 1
+
+    l, r = 0, len(arr) - 1
+
+    while l < r:
+        mid = (r + l) // 2
+        mid_el = arr[mid]
+        if mid_el == target:
+            return mid
+
+        if target < mid_el:
+            r = mid - 1
+        else:
+            l = mid + 1
+
+
+    if arr[l] == target:
+        return l
+    else:
+        return -1
