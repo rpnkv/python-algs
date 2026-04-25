4b22eb6b29c79235c666a396f1c7e5a33295aa1a
s.repnikov <srpnkv.dev@gmail.com>
2026-04-25 08:37:06 +0300

    leetcode:p108:sorted arr to bst: working on, call stack overflow

--- leet/src/problems/p108_sorted_arr_to_bst/solution.py
+++ leet/src/problems/p108_sorted_arr_to_bst/solution.py
@@ -4,8 +4,17 @@ from common.tree_node import TreeNode
 
 
 class Solution:
-
     def sortedArrayToBST(self, nums: List[int], start: int = 0, end: int = None) -> Optional[TreeNode]:
-        the_end = len(nums) if not end else end
+        the_end = len(nums) - 1 if not end else end
 
-        mid =
+        if start == the_end:
+            return TreeNode(nums[start])
+
+        mid = the_end + start // 2
+        return TreeNode(
+            nums[mid],
+            self.sortedArrayToBST(nums, start, mid - 1),
+            self.sortedArrayToBST(nums, mid + 1, the_end)
+        )
+
+
--- leet/test/problems/p108_sorted_arr_to_bst/test.py
+++ leet/test/problems/p108_sorted_arr_to_bst/test.py
@@ -5,15 +5,13 @@ from problems.p108_sorted_arr_to_bst.solution import Solution
 
 TEST_CASES = [
     # pytest.param([-10, -3, 0, 5, 9], [0, -3, 9, -10, None, 5], id="Example 1"),
-    pytest.param([1, 3], [3, 1, None], id="Example 2"),
+    pytest.param([1, 3, None], [3, 1, None], id="Example 2"),
 ]
 
 
 @pytest.mark.parametrize(["input_sorted", "output_level_order"], TEST_CASES)
 def test(input_sorted: list[int], output_level_order: list[int]):
     assert (
-            Solution().sortedArrayToBST(
-                TreeNode.from_level_order_array(input_sorted)
-            ) ==
+            Solution().sortedArrayToBST(input_sorted) ==
             TreeNode.from_level_order_array(output_level_order)
     )
