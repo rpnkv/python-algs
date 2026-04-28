1d8126f0987761c2b788aab8e544233f453a5114
s.repnikov <srpnkv.dev@gmail.com>
2026-04-28 18:30:54 +0300

    leetcode:p450:delete node in a list: backbone

---
+++ leet/src/problems/p450_delete_node_in_bst/solution.py
@@ -0,0 +1,8 @@
+from typing import Optional
+
+from common.tree_node import TreeNode
+
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        raise NotImplementedError
---
+++ leet/test/problems/p450_delete_node_in_bst/test.py
@@ -0,0 +1,6 @@
+from problems.p450_delete_node_in_bst.solution import Solution
+
+
+def test():
+    Solution().deleteNode(None)
+    raise NotImplementedError
