fe83c2e1de6b6ed4fcdf8c6e86590d98df852b40
s.repnikov <srpnkv.dev@gmail.com>
2026-04-29 20:18:44 +0300

    leetcode:p450:delete node in a BST: works for 2/3 cases

--- leet/src/problems/p450_delete_node_in_bst/solution.py
+++ leet/src/problems/p450_delete_node_in_bst/solution.py
@@ -5,41 +5,53 @@ from common.tree_node import TreeNode
 
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        if not root:
-            return None
+        dummy = TreeNode(10, left=root)
 
-        def most_left(root) -> TreeNode:
-            return root
+        def find_parent(target: int):
+            parent = dummy
 
-        def most_right(root) -> TreeNode:
-            return root
+            while parent:
+                if parent.left and parent.left.val == target:
+                    return parent, parent.left
 
-        def search() -> TreeNode:
-            node = root
+                if parent.right and parent.right.val == target:
+                    return parent, parent.right
 
-            while node:
-                if node.val == key:
-                    break
-
-                if key < node.val:
-                    node = node.left
+                if target < parent.val:
+                    parent = parent.left
                 else:
-                    node = node.right
-            return node
+                    parent = parent.right
 
-        removing_node, parent = search()
+            return parent, None
 
-        if not removing_node:
-            return root
+        parent, target = find_parent(key)
 
-        if not removing_node.left or not removing_node.right:
-            if not removing_node.left:
-                removing_node.val = removing_node.right.val
-                removing_node.right = removing_node.right.right
-                removing_node.left = removing_node.right.left
+        if not target:
+            return dummy.left
+
+
+        # if removing node has no children
+        if not target.left and not target.right:
+            if parent.left.val == key:
+                parent.left = None
+                return dummy.left
+            elif parent.right.val == key:
+                parent.right = None
+                return dummy.left
+
+        def substitute(parent, target_val, target_node):
+            if parent.left.val == target_val:
+                parent.left = target_node
             else:
-                removing_node.val = removing_node.left.val
-                removing_node.right = removing_node.left.right
-                removing_node.left = removing_node.left.left
+                parent.right = target_node
 
-        return root
+        # if removing node has 1 child (simple substitution resolution)
+        if not target.left or not target.right:
+            if target.left:
+                substitute(parent, key, target.left)
+                return dummy.left
+            else:
+                substitute(parent, key, target.right)
+                return dummy.left
+
+        raise NotImplementedError
