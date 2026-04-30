fa978ecd717754e416dd0ab5d2c57e2ad26f5f6e
s.repnikov <srpnkv.dev@gmail.com>
2026-04-30 12:15:11 +0300

    leetcode:p724: find pivot element: done

--- leet/src/problems/p724_find_pivot_index/solution.py
+++ leet/src/problems/p724_find_pivot_index/solution.py
@@ -2,9 +2,13 @@ from typing import List
 
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
-        l_sum = r_sum = 0
+        total = sum(nums)
 
-        while l < r:
-            raise NotImplementedError
+        l_sum = 0
 
+        for i, el in enumerate(nums):
+            if total - l_sum - el == l_sum:
+                return i
+
+            l_sum += el
+        return -1
--- leet/test/problems/p724_find_pivot_index/test.py
+++ leet/test/problems/p724_find_pivot_index/test.py
@@ -3,6 +3,7 @@ import pytest
 from problems.p724_find_pivot_index.solution import Solution
 
 TEST_CASES = [
+    #pytest.param([1], 3, id="My example 1"),
     pytest.param([1, 7, 3, 6, 5, 6], 3, id="Example 1"),
     pytest.param([1, 2, 3], -1, id="Example 2"),
     pytest.param([2, -1, 1], 0, id="Example 3"),
