071da50997e53a7de768fc20ae138d0b5fd03ba3
s.repnikov <srpnkv.dev@gmail.com>
2026-05-05 14:59:00 +0300

    leetcode:p206: reverse linked list: added examples as test cases

--- leet/src/problems/p206_reverse_linked_list/solution.py
+++ leet/src/problems/p206_reverse_linked_list/solution.py
@@ -5,4 +5,21 @@ from common.list_node import ListNode
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        raise NotImplementedError
+        if not head:
+            return None
+
+        if not head.next:
+            return head
+
+        if not head.next.next:
+            rev_head = head.next
+            head.next = None
+            rev_head.next = head
+            return rev_head
+
+        new_tail = head
+        switching = head.next
+        new_switching = head.next.next
+
+        while True:
+            raise NotImplementedError
--- leet/test/problems/p206_reverse_linked_list/test_solution.py
+++ leet/test/problems/p206_reverse_linked_list/test_solution.py
@@ -1,8 +1,18 @@
+import pytest
+
 from common.list_node import ListNode
 from problems.p206_reverse_linked_list.solution import Solution
 
+TEST_CASES = [
+    pytest.param([1, 2, 3, 4, 5], [5, 4, 3, 2, 1], id="Example1"),
+    pytest.param([1, 2], [2, 1], id="Example2"),
+]
 
-def test():
-    head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
 
-    Solution().reverseList(head)
+@pytest.mark.parametrize(
+    ["incoming", "expected_outcome"], TEST_CASES
+)
+def test(incoming: list[int], expected_outcome: list[int]):
+    incoming_list = ListNode.to_linked_list(incoming)
+    expected_outcome_list = ListNode.to_linked_list(expected_outcome)
+    assert Solution().reverseList(incoming_list) == expected_outcome_list
