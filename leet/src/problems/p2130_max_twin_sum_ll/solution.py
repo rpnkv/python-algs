8c9eefba3e9ff781791775675150a201868d648e
s.repnikov <srpnkv.dev@gmail.com>
2026-05-05 21:35:55 +0300

    leetcode:p2130:maximum twin sum of a linked list: works

--- leet/src/problems/p2130_max_twin_sum_ll/solution.py
+++ leet/src/problems/p2130_max_twin_sum_ll/solution.py
@@ -5,4 +5,24 @@ from common.list_node import ListNode
 
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        raise NotImplementedError
+        pre_slow = None
+        slow = head
+        fast = head
+
+
+        while fast and fast.next:
+            fast = fast.next.next
+
+            next = slow.next
+            slow.next = pre_slow
+            pre_slow = slow
+            slow = next
+
+        max_sum = 0
+
+        while pre_slow:
+            max_sum = max(pre_slow.val + slow.val, max_sum)
+
+            pre_slow = pre_slow.next
+            slow = slow.next
+        return max_sum
--- leet/test/problems/p2130_max_twin_sum_ll/test.py
+++ leet/test/problems/p2130_max_twin_sum_ll/test.py
@@ -1,2 +1,15 @@
-def test():
-    raise NotImplementedError
+import pytest
+
+from common.list_node import ListNode
+from problems.p2130_max_twin_sum_ll.solution import Solution
+
+TEST_CASES = [
+    pytest.param([1, 2], 3, id="My case 1"),
+    pytest.param([1, 2, 3, 4], 5, id="My case 2"),
+    pytest.param([1, 2, 5, 3, 4, 2], 8, id="My case 3"),
+]
+
+@pytest.mark.parametrize(["incoming", "expected_outcome"], TEST_CASES)
+def test(incoming: list[int], expected_outcome: int):
+    incoming_list = ListNode.to_linked_list(incoming)
+    assert Solution().pairSum(incoming_list) == expected_outcome
