2293b81475bb0ce060dba2b3a0244a818f3e165d
s.repnikov <srpnkv.dev@gmail.com>
2026-05-09 18:03:54 +0300

    leetcode:p345:reverse vowels of a string: done

--- leet/src/problems/p345_reverse_vowels_of_a_string/solution.py
+++ leet/src/problems/p345_reverse_vowels_of_a_string/solution.py
@@ -1,14 +1,21 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
         l, r = 0, len(s) - 1
+        res = [" "] * len(s)
         vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
-        raise NotImplementedError
-        while l < r:
-            if s[l] in wovels:
-                while s[r] not in wovels and r > l:
+
+        while l <= r:
+            if s[l] in vowels:
+                while s[r] not in vowels and r > l:
+                    res[r] = s[r]
                     r -= 1
 
-                s[l], s[r] = s[l], s[r]
-                l += 1
+                res[l] = s[r]
+                res[r] = s[l]
+                r-=1
 
-        return s
+            else:
+                res[l] = s[l]
+            l += 1
+
+        return "".join(res)
