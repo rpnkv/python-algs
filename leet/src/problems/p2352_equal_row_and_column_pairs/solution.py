19a0de1af5e0626376d904d5359c822e6afea757
Sergei Repnikov <theierus@gmail.com>
2026-05-10 14:49:35 +0300

    leetcode:p2352:equal row and column pairs: works for example 1

--- leet/src/problems/p2352_equal_row_and_column_pairs/solution.py
+++ leet/src/problems/p2352_equal_row_and_column_pairs/solution.py
@@ -1,5 +1,15 @@
 from typing import List
 
+
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        raise NotImplementedError
+        columns = {tuple(arr): arr for arr in grid}
+        matching_count = 0
+
+        for row_index in range(len(grid)):
+            row = tuple(col[row_index]for col in grid)
+
+            if row in columns and columns[row] == list(row):
+                matching_count += 1
+
+        return matching_count
