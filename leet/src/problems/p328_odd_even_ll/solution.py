115cc657c91bbb1ca14899208ea323a34e2c7680
s.repnikov <srpnkv.dev@gmail.com>
2026-05-16 10:47:39 +0300

    leetcode: p328 odd even linked list: working on

--- leet/src/problems/p328_odd_even_ll/solution.py
+++ leet/src/problems/p328_odd_even_ll/solution.py
@@ -5,4 +5,18 @@ from common.list_node import ListNode
 
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        raise NotImplementedError
+        if not head or not head.next:
+            return head
+
+        odd_head = head
+        even_head = head.next
+
+        odd = head
+        even = even_head
+
+        while True:
+
+
+
+        odd.next = even_head
+        return odd_head
