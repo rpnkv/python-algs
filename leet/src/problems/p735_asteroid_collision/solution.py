27c4a6309b9972d3dd4e296f2b4da33413a95a33
s.repnikov <srpnkv.dev@gmail.com>
2026-05-17 13:55:54 +0300

    leetcode: p735 asteroid collision: works for non-consecutive collisions

--- leet/src/problems/p735_asteroid_collision/solution.py
+++ leet/src/problems/p735_asteroid_collision/solution.py
@@ -2,4 +2,29 @@ from typing import List
 
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        raise NotImplementedError
+        stack = asteroids[:1]
+
+        for i in asteroids[1:]:
+            if not stack:
+                stack.append(i)
+                continue
+
+            if stack[-1] < 0 and i < 0:
+                stack.append(i)
+                continue
+
+            if stack[-1] > 0 and i > 0 :
+                stack.append(i)
+                continue
+
+            while True:
+                prev = stack.pop()
+                res = prev + i
+                if res != 0:
+                    if prev < 0 and res < 0:
+                        stack.append(prev)
+                    else:
+                        stack.append(i)
+                break
+
+        return stack
--- leet/test/problems/p735_asteroid_collision/test.py
+++ leet/test/problems/p735_asteroid_collision/test.py
@@ -3,12 +3,22 @@ import pytest
 from problems.p735_asteroid_collision.solution import Solution
 
 TEST_CASES = [
+    # my cases
+    pytest.param([5, 10, 2], [5, 10, 2], id="Positives only"),
+    pytest.param([-5, -10, -2], [-5, -10, -2], id="Negatives only"),
+    pytest.param([-5, 5], [], id="Mutual destruction collision"),
+    pytest.param([-6, 5], [-6], id="Stack wins collision"),
+    pytest.param([-4, 5], [5], id="Incoming wins collision"),
+
+
+    # leetcode cases
     pytest.param([5, 10, -5], [5, 10], id="Example 1"),
     pytest.param([8, -8], [], id="Example 2"),
     pytest.param([10, 2, -5], [10], id="Example 3"),
     pytest.param([3, 5, -6, 2, -1, 4], [-6, 2, 4], id="Example 4"),
 ]
 
+
 @pytest.mark.parametrize(["incoming", "expected_outcome"], TEST_CASES)
-def test(incoming:list[int], expected_outcome:list[int]):
+def test(incoming: list[int], expected_outcome: list[int]):
     assert Solution().asteroidCollision(incoming) == expected_outcome
