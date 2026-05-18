d1ca1942f7f6574a3061cbe5342d03604692449b
s.repnikov <srpnkv.dev@gmail.com>
2026-05-18 16:11:37 +0300

    leetcode: p746 min cost climbing stairs: non-optimized recursive solution

--- leet/src/problems/p746_min_cost_climbing_stairs/solution.py
+++ leet/src/problems/p746_min_cost_climbing_stairs/solution.py
@@ -3,4 +3,19 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        raise NotImplementedError
+        cost_cache: dict[int, int] = {}
+
+        def climb(pos: int, curr_cost: int) -> int:
+            if pos >= len(cost):
+                return curr_cost
+            else:
+                cost1 = climb(pos + 1, cost[pos] + curr_cost)
+                cost2 = climb(pos + 2, cost[pos] + curr_cost)
+                return min(
+                    cost1, cost2
+                )
+
+        climb0 = climb(0, 0)
+        climb1 = climb(1, 0)
+
+        return min(climb0, climb1)
