078b10a16c232da420446d3df3eb8414352c12ac
s.repnikov <srpnkv.dev@gmail.com>
2026-05-18 13:01:58 +0300

    leetcode: p735 asteroid collision: works, but looks like may be optimized

--- leet/src/problems/p735_asteroid_collision/solution.py
+++ leet/src/problems/p735_asteroid_collision/solution.py
@@ -4,27 +4,50 @@ class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = asteroids[:1]
 
-        for i in asteroids[1:]:
+        def has_same_sign(i: int, k:int) -> bool:
+            return i < 0 and k < 0
+
+        def will_collide(i: int) -> bool:
+            return i < 0 and stack[-1] > 0
+
+        for a in asteroids[1:]:
             if not stack:
-                stack.append(i)
+                stack.append(a)
                 continue
 
-            if stack[-1] < 0 and i < 0:
-                stack.append(i)
+            if not will_collide(a):
+                stack.append(a)
                 continue
 
-            if stack[-1] > 0 and i > 0 :
-                stack.append(i)
-                continue
 
-            while True:
-                prev = stack.pop()
-                res = prev + i
-                if res != 0:
-                    if prev < 0 and res < 0:
-                        stack.append(prev)
-                    else:
-                        stack.append(i)
-                break
+            # new asteroid, moving in opposite direction than stack can affect
+            # stack i 3 ways:
+            # - be destroyed by 1st el;
+            # - be destroued by Nth el;
+            # - destroy stack's 1st el;
+            # - destroy stack's el until Nth;
+            # - destroy stack content completely.
+
+            while stack and will_collide(a):
+                collided = a + stack[-1]
+
+                if collided == 0:
+                    stack.pop()
+                    break
+
+                if has_same_sign(collided, a):  # new asteroid wins
+                #if collided > 0:  # new asteroid wins
+                    stack.pop()                     # keep colliding
+                    if not stack or not will_collide(a):                   # add element if stack
+                        stack.append(a)             # got empty while this loop
+                        break
+                else:
+                    break
+                    pass # stack asteroid wins, keep going, stack condition
+                    # will become false
 
         return stack
+
+
+
+
--- leet/test/problems/p735_asteroid_collision/test.py
+++ leet/test/problems/p735_asteroid_collision/test.py
@@ -6,16 +6,18 @@ TEST_CASES = [
     # my cases
     pytest.param([5, 10, 2], [5, 10, 2], id="Positives only"),
     pytest.param([-5, -10, -2], [-5, -10, -2], id="Negatives only"),
-    pytest.param([-5, 5], [], id="Mutual destruction collision"),
-    pytest.param([-6, 5], [-6], id="Stack wins collision"),
-    pytest.param([-4, 5], [5], id="Incoming wins collision"),
-
-
+    pytest.param([5, -5], [], id="Mutual destruction collision"),
+    pytest.param([-6, 5], [-6, 5], id="Stack wins collision"),
+    pytest.param([-4, 5], [-4, 5], id="Incoming wins collision"),
+    # double collision cases
+    pytest.param([-4, -3, 5], [-4, -3, 5], id="Double collision reversed pos"),
+    pytest.param([4, 3, -5], [-5], id="Double collision reversed neg"),
     # leetcode cases
     pytest.param([5, 10, -5], [5, 10], id="Example 1"),
     pytest.param([8, -8], [], id="Example 2"),
     pytest.param([10, 2, -5], [10], id="Example 3"),
     pytest.param([3, 5, -6, 2, -1, 4], [-6, 2, 4], id="Example 4"),
+    pytest.param([-2, -2, 1, -2], [-2, -2, -2], id="Case 250"),
 ]
 
 
