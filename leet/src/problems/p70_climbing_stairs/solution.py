73ac69aa496cdbada9f950ba6d665cabd0a63a8c
s.repnikov <srpnkv.dev@gmail.com>
2026-05-18 15:41:21 +0300

    leetcode: p70 climbing stairs: recursive + memoization

--- leet/src/problems/p70_climbing_stairs/solution.py
+++ leet/src/problems/p70_climbing_stairs/solution.py
@@ -1,9 +1,20 @@
 class Solution:
+
+    def __init__(self):
+        self.results: dict[int, int] = {}
+
     def climbStairs(self, n: int, s: int = 0) -> int:
+        if s in self.results:
+            return self.results[s]
+
         if n == s:
+            self.results[s] = 1
             return 1
 
         if n < s:
+            self.results[s] = 0
             return 0
         else:
-            return self.climbStairs(n, s + 1) + self.climbStairs(n, s + 2)
+            curr_res = self.climbStairs(n, s + 1) + self.climbStairs(n, s + 2)
+            self.results[s] = curr_res
+            return curr_res
