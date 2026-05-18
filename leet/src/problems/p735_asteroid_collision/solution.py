388215eb7834a69dfa6845fcdeba9a2eaa422fff
s.repnikov <srpnkv.dev@gmail.com>
2026-05-18 13:15:40 +0300

    leetcode: p735 asteroid collision: works

--- leet/src/problems/p735_asteroid_collision/solution.py
+++ leet/src/problems/p735_asteroid_collision/solution.py
@@ -4,11 +4,8 @@ class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = asteroids[:1]
 
-        def has_same_sign(i: int, k:int) -> bool:
-            return i < 0 and k < 0
-
         def will_collide(i: int) -> bool:
-            return i < 0 and stack[-1] > 0
+            return i < 0 < stack[-1]
 
         for a in asteroids[1:]:
             if not stack:
@@ -19,32 +16,18 @@ class Solution:
                 stack.append(a)
                 continue
 
-
-            # new asteroid, moving in opposite direction than stack can affect
-            # stack i 3 ways:
-            # - be destroyed by 1st el;
-            # - be destroued by Nth el;
-            # - destroy stack's 1st el;
-            # - destroy stack's el until Nth;
-            # - destroy stack content completely.
-
             while stack and will_collide(a):
-                collided = a + stack[-1]
-
-                if collided == 0:
+                if abs(stack[-1]) == abs(a): #mutual destruction
                     stack.pop()
                     break
 
-                if has_same_sign(collided, a):  # new asteroid wins
-                #if collided > 0:  # new asteroid wins
+                if abs(a) > abs(stack[-1]):  # new asteroid wins
                     stack.pop()                     # keep colliding
                     if not stack or not will_collide(a):                   # add element if stack
                         stack.append(a)             # got empty while this loop
                         break
                 else:
                     break
-                    pass # stack asteroid wins, keep going, stack condition
-                    # will become false
 
         return stack
 
