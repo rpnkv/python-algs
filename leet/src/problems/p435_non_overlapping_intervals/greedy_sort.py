e9bf0e321b58e960db62f0b8e4ccdcb6eb7a82a5
s.repnikov <srpnkv.dev@gmail.com>
2026-05-27 20:45:35 +0300

    leetcode: p435 merge intervals: repeating, doesn't work yet

--- leet/src/problems/p435_non_overlapping_intervals/greedy_sort.py
+++ leet/src/problems/p435_non_overlapping_intervals/greedy_sort.py
@@ -3,14 +3,25 @@ from typing import List
 
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key=lambda x:x[0])
-        res = 0
-        prevEnd = intervals[0][0] - 1
+        intervals.sort(key=lambda lst: lst[0])
 
-        for start, end in intervals:
-            if start >= prevEnd:
-                prevEnd = end
+        if len(intervals) == 1:
+            return 0
+
+        l = removed = 0
+
+        for r, r_int in enumerate(intervals):
+            if r == 0:
+                continue
+
+            if r_int[0] != intervals[l][0] and r_int[1] > intervals[l][1]:
+                l = r
             else:
-                res += 1
-                prevEnd = min(end, prevEnd)
-        return res
+                if intervals[l][1] > r_int[1]:
+                    l = r
+                    removed += 1
+                else:
+                    removed += 1
+                    pass
+
+        return removed
