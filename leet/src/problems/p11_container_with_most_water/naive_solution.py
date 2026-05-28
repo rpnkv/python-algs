394da90aebebebefd8751825f2bde74944e0a75b
s.repnikov <srpnkv.dev@gmail.com>
2026-05-28 10:31:02 +0300

    leetcode: p11 container with most water: naive solution rewind

--- leet/src/problems/p11_container_with_most_water/naive_solution.py
+++ leet/src/problems/p11_container_with_most_water/naive_solution.py
@@ -3,20 +3,13 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        list_len = len(height)
+        max_s = 0
 
-        max_area = 0
+        for l in range(len(height)):
+            for r in range(l + 1, len(height)):
+                max_s = max(
+                    max_s,
+                    (r-l) * min(height[l], height[r])
+                )
 
-        for i in range(list_len):
-            for j in range(i + 1, list_len):
-
-                # current_area = min(height[i], height[j]) *
-                min_height = min(height[i], height[j])
-                current_width = abs(i - j)
-
-                current_area = min_height * current_width
-
-                if current_area > max_area:
-                    max_area = current_area
-
-        return max_area
+        return max_s
