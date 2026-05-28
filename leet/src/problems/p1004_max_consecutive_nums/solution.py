5fb9ccbf7d968f0aa594ddffe5bed6a408270ae4
s.repnikov <srpnkv.dev@gmail.com>
2026-05-28 10:56:25 +0300

    leetcode: p1004: max consecutive nums: optimal solution rewind

--- leet/src/problems/p1004_max_consecutive_nums/solution.py
+++ leet/src/problems/p1004_max_consecutive_nums/solution.py
@@ -1,22 +1,23 @@
 from typing import List
 
+# width may not be required
+
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        max_conseq = 0
-        zeroes = 0
         l = 0
-        width = 0
+        width = -1
 
-        for r, r_el in enumerate(nums):
-            if r_el != 1:
-                zeroes += 1
-                while zeroes > k:
-                    if nums[l] == 0:
-                        zeroes -=1
-                    l+=1
-                    width-=1
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                k -= 1
 
-            width += 1
-            max_conseq = max(max_conseq, width)
+            while k < 0:
+                if nums[l] == 0:
+                    k += 1
 
-        return max_conseq + 1
+                l+= 1
+
+            width = max(width, r - l)
+
+        return width + 1 #?
+
