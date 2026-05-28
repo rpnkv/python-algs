e556c4b2aff67061464fabec2e188fb4c88005de
s.repnikov <srpnkv.dev@gmail.com>
2026-05-28 10:36:53 +0300

    leetcode: p11 container with most water: expected solution rewind

--- leet/src/problems/p11_container_with_most_water/expected_solution.py
+++ leet/src/problems/p11_container_with_most_water/expected_solution.py
@@ -18,27 +18,17 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        max_s = 0
         l, r = 0, len(height) - 1
 
-        max_area = 0
+        while l != r:
+            width = r - l
 
-        while True:
-            current_width = r - l
-            current_height = min(height[l], height[r])
+            max_s = max(max_s, width * min(height[l], height[r]))
 
-            current_area = current_width * current_height
-            max_area = max(current_area, max_area)
-
-            if l + 1 == r:
-                break
-
-            if r -1 == l:
-                break
-
-            #if height[l] + 1 > height[r] - 1:
-            if height[l] > height[r]:
-                r -= 1
-            else:
+            if height[l] <= height[r]:
                 l += 1
+            else:
+                r -= 1
 
-        return max_area
+        return max_s
