9cf3825f67c5ae4308742438d5fd8c4f7572b315
Sergei Repnikov <theierus@gmail.com>
2026-05-31 12:19:54 +0300

    leet:p300 longest increasing subsequence: backbone

---
+++ leet/src/problems/p300_longest_increasing_subsequence/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        def dfs(nums: list[int], prev: int) -> int:
+            if not nums:
+                return 0
+
+            if nums[0] > prev:
+                curr = 1
+            else:
+                curr = 0
+
+            subseqs = []
+
+            for n in range(1, len(nums)):
+                subseqs.append(dfs(nums[1:], nums[0]))
+
+            return max(subseqs) + curr
+
+        return dfs(nums, -1001)
---
+++ leet/test/problems/p300_longest_increasing_subsequence/test.py
@@ -0,0 +1,13 @@
+import pytest
+
+from problems.p300_longest_increasing_subsequence.solution import Solution
+
+TEST_CASES = [
+    pytest.param([9, 1, 4, 2, 3, 3, 7], 4, id="Example 1"),
+    pytest.param([0, 3, 1, 3, 2, 3], 4, id="Example 2"),
+]
+
+
+@pytest.mark.parametrize(["incoming", "expected_outcome"], TEST_CASES)
+def test(incoming: list[int], expected_outcome: int):
+    assert Solution().lengthOfLIS(incoming) == expected_outcome
