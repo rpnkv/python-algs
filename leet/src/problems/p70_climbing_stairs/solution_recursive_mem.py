15d26f5ceaafa91055c50dd81e18d8474014980c
s.repnikov <srpnkv.dev@gmail.com>
2026-06-01 21:08:23 +0300

    leetcode: 70 climbing stairs: recursive solution

--- leet/src/problems/p70_climbing_stairs/solution_recursive_mem.py
+++ leet/src/problems/p70_climbing_stairs/solution_recursive_mem.py
@@ -1,11 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp: list[int | None] = [None] * n
-        for i in range(min(n, 2)):
-            dp[i] = i
+        dp = {}
 
         def dfs(n) -> int:
-            if not dp[n]:
+            if n < 3:
+                return n
+
+            if not n in dp:
                 dp[n] = dfs(n - 1) + dfs(n - 2)
             return dp[n]
 
