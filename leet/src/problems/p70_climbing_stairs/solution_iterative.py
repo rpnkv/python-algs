23e9e9c12b0a517c4ca6555a86dc01a3bb28addc
s.repnikov <srpnkv.dev@gmail.com>
2026-06-01 20:51:52 +0300

    leetcode: 70 climbing stairs iterative: rewind

--- leet/src/problems/p70_climbing_stairs/solution_iterative.py
+++ leet/src/problems/p70_climbing_stairs/solution_iterative.py
@@ -1,6 +1,11 @@
 class Solution:
     def climbStairs(self, n: int, c: int = 0) -> int:
-        if c >= n:
-            return 1
-        else:
-            return self.climbStairs(n, c + 1) + self.climbStairs(n, c + 2)
+        steps = [0] * n
+
+        for i in range(n):
+            if i in [0, 1]:
+                steps[i] =i + 1
+            else:
+                steps[i] = sum(steps[i - 2:i])
+
+        return steps[-1]
