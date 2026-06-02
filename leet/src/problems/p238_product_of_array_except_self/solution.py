4cc9c13a063eda8f6cb12c80e420178c5a7ccc08
s.repnikov <srpnkv.dev@gmail.com>
2026-06-02 14:15:52 +0300

    leetcode: 239 product of array except self: rewind

--- leet/src/problems/p238_product_of_array_except_self/solution.py
+++ leet/src/problems/p238_product_of_array_except_self/solution.py
@@ -3,19 +3,21 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pref = [1] * len(nums)
-
+        # ex1: [1, 1, 2, 8]
+        l_to_r = [1] * len(nums)
         for i in range(1, len(nums)):
-            pref[i] = pref[i - 1] * nums[i - 1]
+            l_to_r[i] = l_to_r[i - 1] * nums[i - 1]
 
-        post = [1] * len(nums)
 
+        # ex2: [48, 24, 6, 1]
+        r_to_l = [1] * len(nums)
         for i in range(len(nums) - 2, -1, -1):
-            post[i] = post[i + 1] * nums[i + 1]
+            r_to_l[i] = r_to_l[i + 1] * nums[i + 1]
+
 
         res = []
 
         for i in range(len(nums)):
-            res.append(pref[i] * post[i])
+            res.append(l_to_r[i] * r_to_l[i])
 
         return res
