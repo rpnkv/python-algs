f1a118c3da10909fb78c4ac0e1a7d70055c740a9
s.repnikov <srpnkv.dev@gmail.com>
2026-06-03 21:41:32 +0300

    leetcode: 21 merge two sorted lists: test refator before rewind

--- leet/src/problems/p21_merge_two_sorted_lists/merge_two_sorted_lists.py
+++ leet/src/problems/p21_merge_two_sorted_lists/merge_two_sorted_lists.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from common.list_node import ListNode, to_linked_list
+from common.list_node import ListNode
 
 
 class Solution:
@@ -20,40 +20,29 @@ class Solution:
 
         if list1.val < list2.val:
             head = ListNode(list1.val)
-            list1=list1.next
+            list1 = list1.next
         else:
             head = ListNode(list2.val)
-            list2=list2.next
+            list2 = list2.next
 
-        tail=head
+        tail = head
 
         while list1 is not None and list2 is not None:
             if list1.val < list2.val:
-                next_tail=ListNode(list1.val)
-                tail.next=next_tail
-                list1=list1.next
-                tail=next_tail
+                next_tail = ListNode(list1.val)
+                tail.next = next_tail
+                list1 = list1.next
+                tail = next_tail
             else:
-                next_tail=ListNode(list2.val)
-                tail.next=next_tail
-                tail=next_tail
-                list2=list2.next
+                next_tail = ListNode(list2.val)
+                tail.next = next_tail
+                tail = next_tail
+                list2 = list2.next
 
         if list1 is None and list2 is not None:
-            tail.next=list2
+            tail.next = list2
 
         if list2 is None and list1 is not None:
-            tail.next=list1
+            tail.next = list1
 
         return head
-
-
-def main():
-    sol = Solution()
-    assert sol.mergeTwoLists(list1=to_linked_list([1, 2, 4]), list2=to_linked_list([1, 3, 4])) == to_linked_list(
-        [1, 1, 2, 3, 4, 4]
-    )
-
-
-if __name__ == "__main__":
-    main()
---
+++ leet/test/problems/p21_merge_two_sorted_lists/test.py
@@ -0,0 +1,18 @@
+import pytest
+
+from common.list_node import ListNode
+from problems.p21_merge_two_sorted_lists.merge_two_sorted_lists import Solution
+
+TEST_CASES = [
+    pytest.param([1, 2, 4], [1, 3, 5], [1, 1, 2, 3, 4, 5], id="Example 1"),
+    pytest.param([], [1, 3, 5], [1, 3, 5], id="Example 2"),
+    pytest.param([], [], [], id="Example 3"),
+]
+
+
+@pytest.mark.parametrize(["incoming1", "incoming2", "expected_outcome"], TEST_CASES)
+def test(incoming1: list[int], incoming2: list[int], expected_outcome: list[int]):
+    assert Solution().mergeTwoLists(
+        ListNode.to_linked_list(incoming1),
+        ListNode.to_linked_list(incoming2),
+    ) == ListNode.to_linked_list(expected_outcome)
