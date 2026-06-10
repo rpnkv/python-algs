c7f5967b2b36ab3d2f7ce9bfe55e625f6283730e
s.repnikov <srpnkv.dev@gmail.com>
2026-06-10 11:24:03 +0300

    leetcode: 21 merge two sorted linked lists: rewind

--- leet/test/problems/p21_merge_two_sorted_lists/test.py
+++ leet/test/problems/p21_merge_two_sorted_lists/test.py
@@ -6,6 +6,7 @@ from problems.p21_merge_two_sorted_lists.merge_two_sorted_lists import Solution
 TEST_CASES = [
     pytest.param([1, 2, 4], [1, 3, 5], [1, 1, 2, 3, 4, 5], id="Example 1"),
     pytest.param([], [1, 3, 5], [1, 3, 5], id="Example 2"),
+    pytest.param([1, 3, 5], [], [1, 3, 5], id="Example 2 mirrored"),
     pytest.param([], [], [], id="Example 3"),
 ]
 
