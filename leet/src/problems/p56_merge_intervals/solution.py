ebcb42766f2e4f5e09eda9cd59565b8d190af29a
s.repnikov <srpnkv.dev@gmail.com>
2026-06-11 18:46:54 +0300

    leetcode: 56 merge intervals: rewind

--- leet/src/problems/p56_merge_intervals/solution.py
+++ leet/src/problems/p56_merge_intervals/solution.py
@@ -1,32 +1,15 @@
-from typing import List
-
-
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort(key=lambda interval_list: interval_list[0])
+        intervals.sort(key=lambda l: l[0])
 
-        result = []
+        result = [intervals[0]]
 
-        i = 0
-        while i != len(intervals):
-            if i == len(intervals) - 1:
-                result.append(intervals[i])
-                return result
+        for start, end in intervals:
+            prev_start, prev_end = result[-1]
 
-            current_interval = intervals[i]
-
-            current_max_bound = current_interval[1]
-            j = i + 1
-            next_interval = intervals[j]
-
-            while next_interval[0] <= current_max_bound:
-                current_max_bound = max(next_interval[1], current_max_bound)
-                j += 1
-                if j == len(intervals):
-                    break
-                next_interval = intervals[j]
-
-            result.append([current_interval[0], current_max_bound])
-            i = j
+            if start <= prev_end:
+                result[-1][1] = max(prev_end, end)
+            else:
+                result.append([start,end])
 
         return result
