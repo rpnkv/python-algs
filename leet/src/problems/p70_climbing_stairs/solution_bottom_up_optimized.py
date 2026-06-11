c71ddf5baccc3936874c61de7265003bab23789f
s.repnikov <srpnkv.dev@gmail.com>
2026-06-11 18:18:21 +0300

    leetcode: 70 climbing stairs: bottom-up optimized rewind

--- leet/src/problems/p70_climbing_stairs/solution_bottom_up_optimized.py
+++ leet/src/problems/p70_climbing_stairs/solution_bottom_up_optimized.py
@@ -1,9 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        prev, curr = 0,1
+        a, b = 1, 1
 
-        for _ in range(n):
-            prev, curr = curr, (prev + curr)
+        for _ in range(n - 1):
+            a, b = b, a + b
 
-
-        return curr
+        return b
