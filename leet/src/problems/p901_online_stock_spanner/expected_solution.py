ab3d034384a3eb3d8d0a5d3ba5a37a926d87c4f3
s.repnikov <srpnkv.dev@gmail.com>
2026-06-12 15:03:46 +0300

    leetcode: 901: online stock span: rewind

--- leet/src/problems/p901_online_stock_spanner/expected_solution.py
+++ leet/src/problems/p901_online_stock_spanner/expected_solution.py
@@ -7,17 +7,30 @@ class StockSpanner:
 
         if not stack:
             stack.append((price, 1))
-            return 1
-
-        if stack[-1][0] > price:
-            stack.append((price, 1))
-            return 1
         else:
-            len_sum = 0
+            count = 1
             while stack and stack[-1][0] <= price:
-                old_price, old_len = stack.pop()
-                len_sum+=old_len
+                count += stack.pop()[1]
+            stack.append((price, count))
 
-            stack.append((price, len_sum + 1))
+        return stack[-1][1]
 
-            return stack[-1][1]
+
+if __name__ == "__main__":
+    cases = [
+        ([5, 5, 6], [1, 2, 3], "my example 1"),
+        ([100, 80, 60, 70, 60, 75, 85], [1, 1, 1, 2, 1, 4, 6], "example 1")
+    ]
+
+    for incoming, expected_outcome, case_id in cases:
+        print()
+    print("checking case: '" + case_id + "':")
+    s = StockSpanner()
+    for i in range(len(incoming)):
+        inc, exp_out = incoming[i], expected_outcome[i]
+
+    act_out = s.next(inc)
+
+    assert act_out == exp_out, "Failed for pos: '" + str(i) + "'"
+
+    print("------ SUCCESS ------")
