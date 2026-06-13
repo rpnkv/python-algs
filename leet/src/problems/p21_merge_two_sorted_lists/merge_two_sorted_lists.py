33739d138927b454810190f6a899601721c6aa97
s.repnikov <srpnkv.dev@gmail.com>
2026-06-13 15:19:19 +0300

    leetcode: p1493 merge two sorted lists: rewind

--- leet/src/problems/p21_merge_two_sorted_lists/merge_two_sorted_lists.py
+++ leet/src/problems/p21_merge_two_sorted_lists/merge_two_sorted_lists.py
@@ -4,23 +4,22 @@ from common.list_node import ListNode
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        res_dummy = ListNode()
-        res_tail = res_dummy
+        res = tail = ListNode()
 
         while list1 and list2:
-            if list1.val < list2.val:
-                res_tail.next = list1
-                list1 = list1.next
-            else:
-                res_tail.next = list2
+            if list1.val > list2.val:
+                tail.next = list2
                 list2 = list2.next
+            else:
+                tail.next = list1
+                list1 = list1.next
 
-            res_tail = res_tail.next
+            tail = tail.next
 
-        if list1 and not list2:
-            res_tail.next = list1
+        if list1:
+            tail.next = list1
 
-        if list2 and not list1:
-            res_tail.next = list2
+        if list2:
+            tail.next = list2
 
-        return res_dummy.next
+        return res.next
---
+++ leet/src/problems/p21_merge_two_sorted_lists/solution_template.py
@@ -0,0 +1,8 @@
+from typing import Optional
+
+from common.list_node import ListNode
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        raise NotImplementedError
