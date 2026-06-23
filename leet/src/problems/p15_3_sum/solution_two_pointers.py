f45b4c011dd3abd114d95685b0e8121c49a987fb
s.repnikov <srpnkv.dev@gmail.com>
2026-06-23 13:01:02 +0300

    working on 3 sum, doesn't work

--- leet/src/problems/p15_3_sum/solution_two_pointers.py
+++ leet/src/problems/p15_3_sum/solution_two_pointers.py
@@ -1,41 +1,29 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        results = []
         nums.sort()
+        res = []
 
-        def result() -> list[int]:
-            return [nums[l], nums[m], nums[r]]
 
         for l in range(len(nums) - 2):
             m, r = l + 1, len(nums) - 1
             while m < r:
-                n_l = nums[l]
-                n_m = nums[m]
-                n_r = nums[r]
+                curr_sum = nums[l] + nums[m] + nums[r]
+                match curr_sum:
+                    case x if x == 0:
+                        if (
+                            not res
+                                or
+                            not (res[-1][0] == nums[l] and res[-1][1] == nums[m])
+                        ):
+                            res.append([nums[l], nums[m], nums[r]])
 
-                curr_sum = n_l + n_m + n_r
-                if curr_sum == 0:
-                    duplicate = False
+                        m += 1
+                    case x if x < 0:
+                        m += 1
+                    case x if x > 0:
+                        r -= 1
 
-                    for prev_l, prev_m, prev_r in reversed(results):
-                        if prev_l != n_l:
-                            break
-
-                        if prev_m == n_m:
-                            duplicate = True
-                            break
-
-                    if not duplicate:
-                        results.append(result())
-
-                    m += 1
-
-                if sum(result()) > 0:
-                    r -= 1
-                else:
-                    m += 1
-
-        return results
+        return res
 
 
 if __name__ == "__main__":
