482290598212fc884325dcfc127e61f12d3dbdd0
Sergei Repnikov <srpnkv.dev@gmail.com>
2026-07-10 00:26:30 +0300

    p434: works

--- leet/src/problems/p434_longest_repeating_character_replacement/solution.py
+++ leet/src/problems/p434_longest_repeating_character_replacement/solution.py
@@ -1,41 +1,65 @@
+class Freq:
+    def __init__(self):
+        self.d = {}
+        self.total = 0
+
+    def append(self, c: str):
+        if c not in self.d:
+            self.d[c] = 0
+
+        self.d[c] += 1
+        self.total += 1
+
+    def remove(self, c: str):
+        if self.d[c] < 2:
+            del self.d[c]
+        else:
+            self.d[c] -= 1
+
+        self.total -= 1
+
+    def count_others(self) -> int:
+        d = self.d
+
+        if len(d) == 1:
+            return 0
+        else:
+            char, freq = "", 0
+            for c1, f1 in d.items():
+                if f1 > freq:
+                    char, freq = c1, f1
+
+            return self.total - freq
+
+    def count_all(self) -> int:
+        return self.total
+
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         l = 0
         ln_max = 0
 
-        chars = {}
-
-        def reload_freq():
-            mfc, fr = "a", 0
-            for mfc_candidate, f_candidate in chars.items():
-                if f_candidate > fr:
-                    mfc, fr = mfc_candidate, f_candidate
-
-            return (mfc, fr)
+        freq = Freq()
 
         for r, c in enumerate(s):
-            if c not in chars:
-                chars[c] = 0
-            chars[c] += 1
+            freq.append(c)
 
-            most_freq_char, freq = reload_freq()
+            while freq.count_others() > k:
+                freq.remove(s[l])
+                l += 1
 
-            while freq + k >= r - l:
-                to_remove_char = s[l]
-                chars[to_remove_char] -=1
-                l+=1
-                most_freq_char, freq = reload_freq()
-
-            ln_max = max(ln_max, r - l + 1)
+            ln_max = max(ln_max, freq.count_all())
 
         return ln_max
 
 
-
-
 if __name__ == "__main__":
     cases = [
-        ("XYYX", 2, 4, "ex 1"),
+        # ("XYY", 1, 3, "my 1"),
+        # ("XXYY", 1, 3, "my 2"),
+        ("XXXYY", 1, 4, "my 3"),
+        # ("XYYX", 2, 4, "ex 1"),
         ("AAABABB", 1, 5, "ex 2")
     ]
 
