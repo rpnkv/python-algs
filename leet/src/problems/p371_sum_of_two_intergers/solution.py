33343ef228ff31fc4b7898b128e6f57f43026816
s.repnikov <srpnkv.dev@gmail.com>
2026-07-11 14:41:20 +0300

    leetcode: p371: sum of two integers: looks like works for positive ints, but fails on negative

--- leet/src/problems/p371_sum_of_two_intergers/solution.py
+++ leet/src/problems/p371_sum_of_two_intergers/solution.py
@@ -3,10 +3,9 @@ class Solution:
         res = 0
         mask = 0b01
 
-        carry = 0
+        carry = place = 0
 
         while a or b or carry:
-            #res = res << 1
             a_bit = a & mask
             b_bit = b & mask
 
@@ -19,30 +18,34 @@ class Solution:
             elif (a_bit | b_bit) & carry:
                 carry = 0b01
                 res_mask = 0b00
-            elif a | b_bit | carry:
+            elif a_bit | b_bit | carry:
                 carry = 0b00
                 res_mask = 0b01
             else:
                 carry = 0b00
                 res_mask = 0b00
 
-            #res = res | res_mask
-            res |= (1 << i)
+            res_mask <<= place
 
-            a = a >> 1
-            b = b >> 1
+            res |= res_mask
+
+            a >>= 1
+            b >>= 1
+            place += 1
 
         return res
 
 
 if __name__ == "__main__":
     cases = [
-        #(1,2,3, "ex 1N"),
-        (1,1,2, "ex 1L"),
-        #(2,3,5, "ex 2")
+        (1, 2, 3, "ex 1N"),
+        (4, 7, 11, "ex 2N"),
+        (1, 2, 3, "ex 1L"),
+        (2, 3, 5, "ex 2L"),
+        (4, 5, 9, "cs 6L"),
     ]
 
-    for i1, i2, e, case_id in cases:
-        a = Solution().getSum(i1,i2)
+    for i1, i2, e, case_id in cases[4:]:
+        a = Solution().getSum(i1, i2)
 
         assert a == e, f"failed case {case_id}; a/e: {a}/{e}"
