f630a2ad6dc72f7f620a01f0df37082906ffba9d
s.repnikov <srpnkv.dev@gmail.com>
2026-07-24 18:49:43 +0300

    leetcode: p1143 longest common subsequence: top down dp

--- leet/src/problems/p1143_longest_common_subsequence/solution_recursive_mem.py
+++ leet/src/problems/p1143_longest_common_subsequence/solution_recursive_mem.py
@@ -1,23 +1,31 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        lens = []
+        mem = {}
 
-        def dfs(text1: str, text2: str) -> int:
-            if len(text1) == 0 or len(text2) == 0:
+        def dfs(i1: int, i2: int) -> int:
+            if i1 == len(text1) or i2 == len(text2):
                 return 0
-            m_l = 0
-            for i, c1 in enumerate(text1):
-                for j, c2 in enumerate(text2):
-                    if c1 == c2:
-                        max_len = dfs(text1[i + 1:], text2[j + 1:])
-                        m_l = max(m_l, max_len + 1)
-                        lens.append((i, j, 1 + max_len))
 
-            return m_l
+            # for i in range(i1, len(text1)):
+            #     for j in range(i2, len(text2)):
+            #         if text1[i] == text2[j]:
+            #             if not (i, j) in mem:
+            #                 max_len = max(dfs(i1 + 1, i2 + 1) + 1, 1)
+            #                 mem[(i, j)] = max(mem.get((i, j), 0), max_len)
+            #
+            # return mem.get((i1, i2), 0)
+            if (i1, i2) in mem:
+                return mem[(i1, i2)]
 
-        dfs(text1, text2)
-        l = [t[2]for t in lens]
-        return max(l)
+            if text1[i1] == text2[i2]:
+                mem[(i1, i2)] = dfs(i1 + 1, i2 + 1) + 1
+            else:
+                mem[(i1, i2)] = max(dfs(i1, i2 + 1), dfs(i1 + 1, i2))
+
+            return mem[(i1, i2)]
+
+        dfs(0, 0)
+        return max(mem.values())
 
 
 if __name__ == "__main__":
@@ -33,7 +41,7 @@ if __name__ == "__main__":
          210, "case 18"),
     ]
 
-    for inc1, inc2, exp, case_id in cases[0:]:
+    for inc1, inc2, exp, case_id in cases[:7]:
         act = Solution().longestCommonSubsequence(inc1, inc2)
 
         assert exp == act, f"failed case {case_id}: a/e:{act}/{exp}"
